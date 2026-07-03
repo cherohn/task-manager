05aeae5cae75eab5ff1e8027cad860c561c684e5
cherohn <matheus.garcez09@gmail.com>
2026-07-03 17:58:51 -0300

    adicionado exceptions customizadas

---
+++ src/main/java/com/cherohn/taskManager/exception/ConflictException.java
@@ -0,0 +1,7 @@
+package com.cherohn.taskManager.exception;
+
+public class ConflictException extends RuntimeException {
+    public ConflictException(String message) {
+        super(message);
+    }
+}
