d47d2fd6233bfa63b98ed0b8be050e90402ccebf
cherohn <matheus.garcez09@gmail.com>
2026-07-03 17:05:27 -0300

    adicionado requisicao de DTOs com validacao de constraints

---
+++ src/main/java/com/cherohn/taskManager/dto/RegisterRequest.java
@@ -0,0 +1,24 @@
+package com.cherohn.taskManager.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    @NotBlank(message = "O Nome nao pode estar em branco")
+    private String name;
+
+    @NotBlank(message = "O email nao pode estar em branco")
+    @Email(message = "Email Invalido")
+    private String email;
+
+    @NotBlank(message = "A senha nao pode estar em branco")
+    @Size(min = 12, message = "A senha deve ter no minimo 12 caracteres")
+    private String password;
+}
